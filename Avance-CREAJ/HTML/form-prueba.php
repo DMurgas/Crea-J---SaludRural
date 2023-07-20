0df3675f711065f88d1463bd7d94b20a5e63b520
Alvin Meléndez <136837471+15Cesaro49@users.noreply.github.com>
2023-07-20 15:54:03 -0600

    aaaa
    aaaa messi

--- Avance-CREAJ/HTML/form-prueba.php
+++ Avance-CREAJ/HTML/form-prueba.php
@@ -1,12 +1,11 @@
-<!doctype html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Cita</title>
-  <link rel="stylesheet" href="../CSS/Citas.css">
-  <link rel="shortcut icon" href="../Imagenes/favicon.png" />
-</head>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Formulario Multipasos :: Urian Viera</title>
+    <link rel="stylesheet" href="../CSS/form-donaciones.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+  </head>
 <body>
     <?php
     include("../PHP/conex.php");
@@ -37,19 +36,107 @@
         }
     }
     ?>
-<form class="form" method="POST">
-  <h2>Dinos qué está pasando, así nuestros especialistas podrán darte un mejor servicio</h2>
-  <p>Nombre: <input name="nombre" placeholder="Escribe tu nombre aquí"></p>
-  <p>Edad: <input type="number" name="edad" placeholder="¿Cuál es tu edad?"></p>
-  <p>Mensaje: <input name="problem" placeholder="¿Cuál es el problema que presenta el paciente?"></p>
-  <p>Fecha: <input type="date" name="date" placeholder="Fecha estimada para la consulta"></p>
-  <p><input name="registro" class="boton" type="submit" value="Registrar"></p>
-  <div>
-    <span class="fa fa-phone"></span>001 1023 567
-    <span class="fa fa-envelope-o"></span>dehechoundentista@gmail.com
-  </div>
-</form>
-<button onclick="window.history.back();" class="boton-regresar">Regresar</button>
-</body>
-</html>
+    <div class="container">
+      <header>Realizar una donacion</header>
+      <div class="progress-bar">
+        <div class="step">
+          <p>Paso 1</p>
+          <div class="bullet">
+            <span>1</span>
+          </div>
+          <div class="check fas fa-check"></div>
+        </div>
+        <div class="step">
+          <p>Paso 2</p>
+          <div class="bullet">
+            <span>2</span>
+          </div>
+          <div class="check fas fa-check"></div>
+        </div>
+        <div class="step">
+          <p>Paso 3</p>
+          <div class="bullet">
+            <span>3</span>
+          </div>
+          <div class="check fas fa-check"></div>
+        </div>
+        <div class="step">
+          <p>Fin</p>
+          <div class="bullet">
+            <span>4</span>
+          </div>
+          <div class="check fas fa-check"></div>
+        </div>
+      </div>
+      <div class="form-outer">
+        <form action="#">
+          <div class="page slide-page">
+            <div class="field">
+              <div class="label">Nombre Completo</div>
+              <input type="text" required>
+            </div>
+            <div class="field">
+              <div class="label">Correo Electronico</div>
+              <input type="text" required>
+            </div>
+            <div class="field">
+              <button class="firstNext next">Siguiente</button>
+            </div>
+          </div>
 
+          <div class="page">
+            <div class="title">Información de Contacto</div>
+            <div class="field">
+              <div class="label">Número de Telefono</div>
+              <input type="Number" required>
+            </div>
+            <div class="field">
+              <div class="label">Genero</div>
+              <select>
+                <option>Masculino</option>
+                <option>Femenino</option>
+              </select>
+            </div>
+            <div class="field btns">
+              <button class="prev-1 prev">Atrás</button>
+              <button class="next-1 next">Siguiente</button>
+            </div>
+          </div>
+
+          <div class="page">
+            <div class="field">
+              <div class="label">Fecha de la donacion</div>
+              <input type="datetime-local"required>
+            </div>
+            <div class="field">
+              <div class="label">Monto a donar</div>
+              <input type="number" required>
+            </div>
+            <div class="field btns">
+              <button class="prev-2 prev">Atrás</button>
+              <button class="next-2 next">Siguiente</button>
+            </div>
+          </div>
+
+          <div class="page">
+            <div class="title">Datos de tarjeta</div>
+            <div class="field">
+              <div class="label">Numero de tarjeta</div>
+              <input type="text" required>
+            </div>
+            <div class="field">
+              <div class="label">CVV</div>
+              <input type="password" required>
+            </div>
+            <div class="field btns">
+              <button class="prev-3 prev">Atrás</button>
+              <button class="submit">Enviar</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+    <script src="../JS/donaciones.JS"></script>
+    <button onclick="window.history.back();" class="boton-regresar">Regresar</button>
+  </body>
+</html>
