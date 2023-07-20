cdc3c843e73990d448a3d317de65fccb8bb2581a
15Cesaro49 <136837471+15Cesaro49@users.noreply.github.com>
2023-07-19 19:50:22 -0600

    a

--- Avance-CREAJ/HTML/Index.php
+++ Avance-CREAJ/HTML/Index.php
@@ -12,7 +12,7 @@ if($_SESSION["correo"] === null){
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
     integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd
     1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>SaludRural</title>
@@ -35,7 +35,7 @@ if($_SESSION["correo"] === null){
         </div>
       </div>
 
-      <div class="dropdown_menu open">
+      <div class="dropdown_menu">
         <li><a href="#"><strong>Inicio</strong></a></li>
         <li><a href="#">Donaciones</a></li>
         <li><a href="#">Blog</a></li>
