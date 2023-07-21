df53a510f75ede5bbc12be785a6b8b16663b97f2
TulioGamer503 <105886705+TulioGamer503@users.noreply.github.com>
2023-07-21 13:52:02 -0600

    aaa

---
+++ Avance-CREAJ/HTML/form-mone.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Formulario Multipasos :: Urian Viera</title>
+    <link rel="stylesheet" href="../CSS/form-donaciones.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+  </head>
+<body>
+
+    <div class="container">
+      <header>Realizar una donacion</header>
+      <div class="progress-bar">
+        <div class="step">
+          <p>Paso 1</p>
+          <div class="bullet">
+            <span>1</span>
+          </div>
+          <div class="check fas fa-check"></div>
+        </div>
+        <div class="step">
+          <p>Paso 2</p>
+          <div class="bullet">
+            <span>2</span>
+          </div>
+          <div class="check fas fa-check"></div>
+        </div>
+        <div class="step">
+          <p>Paso 3</p>
+          <div class="bullet">
+            <span>3</span>
+          </div>
+          <div class="check fas fa-check"></div>
+        </div>
+        <div class="step">
+          <p>Fin</p>
+          <div class="bullet">
+            <span>4</span>
+          </div>
+          <div class="check fas fa-check"></div>
+        </div>
+      </div>
+      <div class="form-outer">
+        <form action="../PHP/registrar_donacion_2.php" method="post">
+          <div class="page slide-page">
+            <div class="field">
+              <div class="label">Nombre Completo</div>
+              <input type="text" required name="nombre">
+            </div>
+            <div class="field">
+              <div class="label">Correo Electronico</div>
+              <input type="text" required name="correo">
+            </div>
+            <div class="field">
+              <button class="firstNext next">Siguiente</button>
+            </div>
+          </div>
+
+          <div class="page">
+            <div class="title">Información de Contacto</div>
+            <div class="field">
+              <div class="label">Número de Telefono</div>
+              <input type="Number" required name="telefono">
+            </div>
+            <div class="field">
+            <div class="label">Género</div>
+              <select name="genero">
+                  <option value="Masculino">Masculino</option>
+                  <option value="Femenino">Femenino</option>
+              </select>
+            </div>
+            <div class="field btns">
+              <button class="prev-1 prev">Atrás</button>
+              <button class="next-1 next">Siguiente</button>
+            </div>
+          </div>
+
+          <div class="page">
+            <div class="field">
+              <div class="label">Fecha de la donacion</div>
+              <input type="date"required name="fecha">
+            </div>
+            <div class="field">
+              <div class="label">Monto a donar</div>
+              <input type="number" required name="monto">
+            </div>
+            <div class="field btns">
+              <button class="prev-2 prev">Atrás</button>
+              <button class="next-2 next">Siguiente</button>
+            </div>
+          </div>
+
+          <div class="page">
+            <div class="title">Datos de tarjeta</div>
+            <div class="field">
+              <div class="label">Numero de tarjeta</div>
+              <input type="text" required name="tarjeta">
+            </div>
+            <div class="field">
+              <div class="label">CVV</div>
+              <input type="password" required name="cvv">
+            </div>
+            <div class="field btns">
+              <button class="prev-3 prev">Atrás</button>
+              <button class="submit">Enviar</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+    <script src="../JS/donaciones.JS"></script>
+  </body>
+</html>
--- Avance-CREAJ/PHP/form-donacion-insumos.php
+++ Avance-CREAJ/PHP/form-donacion-insumos.php
@@ -1,30 +1,47 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-    // Establecer la conexión con la base de datos
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "crea-J";
+$db_host = 'localhost';
+$db_username = 'root';
+$db_password = '';
+$db_name = 'saludrural';
+$conn = mysqli_connect($db_host, $db_username, $db_password, $db_name);
 
-$conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
-  die("Error al conectar a la base de datos: " . mysqli_connect_error());
+    die("Error de la conexion a la base de datos: " . mysqli_connect_error());
 }
 
-// Recuperar los datos enviados por el formulario
-$insumo = $_POST['insumo'];
-$correo = $_POST['correo'];
-$telefono = $_POST['telefono'];
-$cantidades = $_POST['cantidades'];
-
-
-    $sql = "INSERT INTO (insumo, correo, telefono, cantidades) VALUES ('$insumo', '$correo', '$telefono','$cantidades')";
-    if (mysqli_query($conn, $sql)) {
-      echo "<script>alert('La donacion ha sido realizada correctamente'); window.location.href='#';</script>";
-    } else {
-      echo "<script>alert('Se ha producido un error durante la donacion vuelve a intentarlo'); window.location.href='#';</script> " . mysqli_error($conn);
+if(!empty($_POST["correo"])){
+    session_start();
+    $usuarioId = $_SESSION['usuario_id'];
+    if (!empty($_POST["nombre"]) && !empty($_POST["correo"])  && !empty($_POST["telefono"])  && !empty($_POST["fecha"]) && !empty($_POST["monto"]) && !empty($_POST["tarjeta"]) && !empty($_POST["cvv"])) {
+        $nombre = $_POST["nombre"];
+        #$correo = $_POST["correo"];
+        #$contacto = $_POST["info"];
+        $telefono = $_POST["telefono"];
+        $genero = $_POST["genero"];
+        $fecha = $_POST["fecha"];
+        $monto = $_POST["monto"];
+        $tarjeta = $_POST["tarjeta"];
+        $cvv = $_POST["cvv"];
+    
+        // Insertar la donación asociada al usuario ID
+        $sql = $conn->query ("INSERT INTO `donacion_insumos`(`ID`, `usuario_id`, `nombre`, `correo`, `telefono`, `genero`, `fecha`, `monto`, `tarjeta`, `cvv`) VALUES (NULL, '$usuarioId', '$nombre', '$genero', '$fecha', '$monto', '$tarjeta', '$cvv')");
+    
+        // Ejecutar la consulta
+        if ($sql) {
+            echo '<script language="javascript">alert("Donación registrada correctamente"); window.location.href="../HTML/Index.php";</script>';
+            exit; // Salir del script después de mostrar el mensaje de éxito y redirigir
+        } else {
+            echo '<script language="javascript">alert("Error en el registro de la donación: ' . mysqli_error($conn) . '");</script>';
+        }
     }
+}
 
-// Cerrar la conexión con la base de datos
-mysqli_close($conn);
-    ?>
+//}else{
+    #echo '<script language="javascript">alert("Donacion no registrada"); window.location.href="../HTML/form-prueba.php";</script>';
+    exit; // Salir del script después de mostrar el mensaje de éxito y redirigir
+
+?>
