69e9d8bff566fdab045b2d508fb51a6ddcc8c7bd
TulioGamer503 <105886705+TulioGamer503@users.noreply.github.com>
2023-08-05 13:42:16 -0600

    aaaa
    aaaaaa

--- Avance-CREAJ/HTML/AcercaDe.php
+++ Avance-CREAJ/HTML/AcercaDe.php
@@ -1,3 +1,25 @@
+<?php
+session_start();
+error_reporting(0);
+
+// Verificar si el usuario ha iniciado sesión
+if (!isset($_SESSION['correo']) || empty($_SESSION['correo'])) {
+    echo '<script language="javascript">alert("Por favor inicie sesión o regístrese");window.location.href="../HTML/login.php"</script>';
+    die();
+} else {
+    include("../PHP/conex.php");
+
+    // Consulta SQL para obtener el ID del usuario según el correo electrónico
+    $correo = $_SESSION['correo'];
+    $query = "SELECT id FROM registro WHERE correo = '$correo'";
+    $result = $conn->query($query);
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $_SESSION['usuario_id'] = $row['id'];
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="es">
     <head>
@@ -12,37 +34,70 @@
         <link rel="shortcut icon" href="../Imagenes/favicon.png" />
         <title>Acerca De</title>
         <link rel="stylesheet" href="../CSS/AcercaDe.css">
+        <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.3/dist/tailwind.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
     </head>
+    <nav class="bg-white p-4">
+      <div class="flex justify-between items-center">
+          <!-- Logo o nombre del sitio -->
+          <div class="logo"><img src="../Imagenes/logo.png" width="150" height="90"></div>
 
-    <header>
-        <!-- Código html de la barra de navegación. -->
-        <div class="navbar">
-            <div class="logo"><img src="../Imagenes/logo.png" width="200" height="90"></div>
-            <ul class="links">
-              <li><a href="../HTML/Index.html"><strong>Inicio</strong></a></li>
-              <li><a href="#">Donaciones</a></li>
-              <li><a href="#">Blog</a></li>
-              <li><a href="">Acerca de</a></li>
-            </ul>
-            <a href="../PHP/cerrar.php" class="action_btn"><strong>Iniciar sesión</strong></a>
-            <div class="toggle_btn">
-              <i class="fa-solid fa-bars"></i>
-            </div>
+          <!-- Menú de navegación -->
+          <ul class="flex space-x-4">
+              <li><a href="Index.php" class="text-black hover:bg-blue-600 hover:text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Inicio</a></li>
+              <li class="relative">
+                  <!-- Enlace con menú desplegable -->
+                  <a href="#" class="text-black hover:bg-blue-600 hover:text-white rounded-md px-3 py-2 text-sm font-medium" id="donaciones-menu">
+                      <span>Donaciones</span>
+                      <i class="fas fa-chevron-down ml-1"></i> <!-- Flecha hacia abajo -->
+                  </a>
+
+                  <!-- Menú desplegable -->
+                  <ul class="absolute top-10 left-1/2 transform -translate-x-1/2 bg-white shadow-md rounded-md hidden" id="donaciones-menu-items">
+                      <li><a href="#" class="block px-4 py-2 text-gray-800 hover:bg-blue-600 hover:text-white">Medicamentos</a></li>
+                      <li><a href="#" class="block px-4 py-2 text-gray-800 hover:bg-blue-600 hover:text-white">Equipos medicos</a></li>
+                      <li><a href="#" class="block px-4 py-2 text-gray-800 hover:bg-blue-600 hover:text-white">Isumos medicos</a></li>
+                      <li><a href="#" class="block px-4 py-2 text-gray-800 hover:bg-blue-600 hover:text-white">Monetaria</a></li>
+                      <li><a href="donaciones-reali.php" class="block px-4 py-2 text-gray-800 hover:bg-blue-600 hover:text-white">Realizadas</a></li>
+                  </ul>
+              </li>
+              <li><a href="#" class="text-black hover:bg-blue-600 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Blog</a></li>
+              <li><a href="#" class="text-black hover:bg-blue-600 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Nosotros</a></li>
+              <li class="relative">
+                  <!-- Enlace con menú desplegable -->
+                  <a href="#" class="text-black hover:bg-blue-600 hover:text-white rounded-md px-3 py-2 text-sm font-medium" id="hospitales-menu">
+                      <span>Hospitales</span>
+                      <i class="fas fa-chevron-down ml-1"></i> <!-- Flecha hacia abajo -->
+                  </a>
+
+                  <!-- Menú desplegable -->
+                  <ul class="absolute top-10 left-1/2 transform -translate-x-1/2 bg-white shadow-md rounded-md hidden" id="hospitales-menu-items">
+                      <li><a href="#" class="block px-4 py-2 text-gray-800 hover:bg-blue-600 hover:text-white">Necesidades actuales</a></li>
+                      <li><a href="#" class="block px-4 py-2 text-gray-800 hover:bg-blue-600 hover:text-white">Historias de exito</a></li>
+                  </ul>
+              </li>
+          </ul>
+
+          <div class="relative">
+              <button type="button" class="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                  <span class="absolute -inset-1.5"></span>
+                  <span class="sr-only">Open user menu</span>
+                  <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+              </button>
+              <!-- Menú desplegable del usuario -->
+              <ul class="absolute right-0 mt-2 py-2 w-50 bg-white rounded-lg shadow-md hidden" id="user-menu">
+              <?php
+              // Mostrar nombre del usuario si está disponible en la sesión
+              if (isset($_SESSION['correo']) && !empty($_SESSION['correo'])) {
+                  echo '<li><a href="#" class="block px-1 py-2 text-gray-800 hover:bg-blue-600 hover:text-white">' . $_SESSION['correo'] . '</a></li>';
+              }
+              ?>
+              <li><a href="#" class="block px-4 py-2 text-gray-800 hover:bg-blue-600 hover:text-white">Configuración</a></li>
+              <li><a href="../PHP/cerrar.php" class="block px-4 py-2 text-red-600 hover:bg-red-600 hover:text-white">Cerrar Sesión</a></li>
+          </ul>
           </div>
-    
-          <div class="dropdown_menu open ">
-            <li><a href="../HTML/Index.html"><strong>Inicio</strong></a></li>
-            <li><a href="form-mone.php">Donaciones</a></li>
-            <li><a href="#">Blog</a></li>
-            <li><a href="">Acerca de</a></li>
-            <li><a href="../HTML/login.php" class="action_btn"><strong>Iniciar sesión</strong></a></li>
-            <a href="../PHP/cerrar.php" class="action_btn"><strong>Iniciar sesión</strong></a>
-            <div class="toggle_btn">
-              <i class="fa-solid fa-bars"></i>
-            </div>
-          </div>
-    </header>
+  </nav>
 
     <body>
       <!-- Código del slider carrusel -->
@@ -161,4 +216,25 @@
         <p><small id="26">&copy; 2023 <b>SaludRural</b> - Todos los Derechos Reservados.</small></p>
       </div>
     </footer>
+    <script>
+    // Script para mostrar/ocultar el menú desplegable del usuario al hacer clic en el botón del usuario
+    const userMenuButton = document.getElementById('user-menu-button');
+    const userMenu = document.getElementById('user-menu');
+    userMenuButton.addEventListener('click', () => {
+        userMenu.classList.toggle('hidden');
+    });
+
+    // Script para mostrar/ocultar el menú desplegable de donaciones al hacer clic en el botón de donaciones
+    const donacionesMenuButton = document.getElementById('donaciones-menu');
+    const donacionesMenuItems = document.getElementById('donaciones-menu-items');
+    donacionesMenuButton.addEventListener('click', () => {
+        donacionesMenuItems.classList.toggle('hidden');
+    });
+    // Script para mostrar/ocultar el menú desplegable de donaciones al hacer clic en el botón de donaciones
+    const hospitalesMenuButton = document.getElementById('hospitales-menu');
+    const hospitalesMenuItems = document.getElementById('hospitales-menu-items');
+    hospitalesMenuButton.addEventListener('click', () => {
+        hospitalesMenuItems.classList.toggle('hidden');
+    });
+</script>
 </html>
