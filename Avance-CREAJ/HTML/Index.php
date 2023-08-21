5c5a9131e01244c80d90b284e6c22a584e3864c6
CELS09 <lopez.saca.carlos8a@gmail.com>
2023-08-21 14:00:03 -0600

    dfgrgh

--- Avance-CREAJ/HTML/Index.php
+++ Avance-CREAJ/HTML/Index.php
@@ -151,11 +151,7 @@ div .skiptranslate.goog-te-gadget, .goog-te-combo .dark{
                 <li><a href="../PHP/cerrar.php" class="block px-4 py-2 text-red-600 hover:bg-red-600 hover:text-white">Cerrar sesión</a></li>
             </ul>
             </div>
-    </nav>
-
-
-
-
+    </nav><br><br><br>
 
     <main>
     <section class="bg-blue-600 text-white py-24">
