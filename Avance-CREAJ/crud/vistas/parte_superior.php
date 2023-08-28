5b945571932e3aa686157e63a013ebbdaf57c66b
User 1 <136837471+15Cesaro49@users.noreply.github.com>
2023-08-28 14:19:33 -0600

    r

--- Avance-CREAJ/crud/vistas/parte_superior.php
+++ Avance-CREAJ/crud/vistas/parte_superior.php
@@ -201,19 +201,6 @@ div .skiptranslate.goog-te-gadget, .goog-te-combo .dark{
               </a>
               <!-- Dropdown - User Information -->
               <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                <a class="dropdown-item" href="#">
-                  <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                  Perfil
-                </a>
-                <a class="dropdown-item" href="#">
-                  <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                  Ajustes
-                </a>
-                <a class="dropdown-item" href="#">
-                  <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                  Actividades
-                </a>
-                <div class="dropdown-divider"></div>
                 <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                   <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                   Cerrar sesión
